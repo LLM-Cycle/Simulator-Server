d3b5b90959ba114b4ae90e3c8eb41888750f0c63
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-08-12 11:36:25 +0530

    add : prometheus configuration done

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import express from 'express'; 
+import { Request , Response } from 'express';
 import logger from './config/logger'; 
 import requestLogger from './middlewares/ipLogger.middleware';
-const app = express(); 
+import client from 'prom-client';
 
-app.use(requestLogger); 
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics ; 
 
-app.get("/health" , (req , res) => {
+//prom init
+collectDefaultMetrics({register : client.register});
+
+//ipLogger **note** do not remove below line
+// app.use(requestLogger); 
+
+app.get("/health" , (req:Request, res:Response) => {
     logger.info("The server is working all fine and good"); 
     res.json({
         message :"Server is running OK !"
     }); 
 })
 
+app.get("/metrics" , async(req :Request , res:Response) => {
+    res.setHeader("Content-Type" , client.register.contentType);
+    const metrics = await client.register.metrics(); 
+    res.send(metrics);
+});
 
-app.listen(9090 , () => {
-    console.log(`The server is up and is running on PORT 9090`); 
+
+app.listen(8000 , () => {
+    console.log(`The server is up and is running on PORT 8000`); 
 })
 
